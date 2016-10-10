0c010bff46ff4951df11de3f93876927241aec50
Joe Phearse <jphearse@gmail.com>
2016-10-10 15:56:30 -0500

    moved conditional logic outside of view

--- resources/views/roll-dice.php
+++ resources/views/roll-dice.php
@@ -6,10 +6,10 @@
 <body>
 	<h1>Roll Dice: <?= $random ?></h1>
 	<h1>Your Guess: <?= $guess ?></h1>
-	<?php if ($random == $guess) { ?>
-		<h1>Your guess is correct</h1>
+	<?php if ($correct) { ?>
+		<h1>Correct</h1>
 	<?php } else {?>
-		<h1>Your guess is wrong</h1>
+		<h1>Wrong</h1>
 	<?php } ?>
 </body>
 </html>
